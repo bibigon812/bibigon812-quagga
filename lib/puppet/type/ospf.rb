acc47dcd9c31cea6c4a3ac4e4d1c6489bb128dd5
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-01-16 13:16:24 +0300

    Added validate and munge methods for default_information property

--- lib/puppet/type/ospf.rb
+++ lib/puppet/type/ospf.rb
@@ -42,20 +42,87 @@ Puppet::Type.newtype(:ospf) do
   newproperty(:default_information) do
     desc %q{Control distribution of default information.}
 
+    originate_attributes = [:always, :metric, :metric_type, :route_map]
+
+    newvalues(:fasle)
+    newvalues(/\A.*\Z/)
+
+    defaultto(:false)
+
     validate do |value|
-    end
-    
-    munge do |value|
       if value.is_a?(String)
         if value.include?('=>')
           begin
-            eval(value.gsub(/(\w+)/, '\'\1\''))
+            hash = eval(value.gsub(/([\w-]+)/, '\'\1\''))
           rescue SyntaxError => e
-            raise ArgumentError, '%s is not a Hash' % value
+            raise ArgumentError, '\'%s\' is not a Hash' % value
+          end
+          if hash.has_key?('originate')
+            hash['originate'].each_key do |key|
+              unless originate_attributes.include?(key.gsub(/-/, '_').to_sym)
+                raise ArgumentError, '\'%s\' is not valid originate attribute' % key
+              end
+            end
+          else
+            raise ArgumentError, '\'%s\' must be \'originate\'' % hash.keys.first
           end
         else
-          arr = value.split(/\s/)
-          arr[0..-2].reverse.inject(arr.last) { |a, b| {b => a} }
+          array = value.split(/\s+/)
+          unless array.shift == 'originate'
+            raise ArgumentError, 'first word must be \'originate\''
+          end
+          while not array.empty?
+            attribute = array.shift.gsub(/-/, '_').to_sym
+            unless originate_attributes.include?(attribute)
+              raise ArgumentError, '\'%s\' is not a valud originate attribute' % attribute
+            end
+            unless attribute == :always
+              array.shift
+            end
+          end
+        end
+      end
+    end
+
+    munge do |value|
+      if value.is_a?(String)
+        if value.include?('=>')
+          hash = eval(value.gsub(/([\w-]+)/, '\'\1\''))
+          new_hash = {}
+          new_hash[:originate] = {}
+          hash['originate'].each do |key, value|
+            attribute = key.gsub(/-/, '_').to_sym
+            if originate_attributes.include?(attribute)
+              case attribute
+              when :always
+                new_hash[:originate][attribute] = :true
+              when :metric, :metric_type
+                new_hash[:originate][attribute] = value.to_i
+              else
+                new_hash[:originate][attribute] = value
+              end
+            end
+          end
+          new_hash
+        else
+          array = value.split(/\s/)
+          new_hash = {}
+          new_hash[:originate] = {}
+          array.shift
+          while not array.empty?
+            attribute = array.shift.gsub(/-/, '_').to_sym
+            if originate_attributes.include?(attribute)
+              case attribute
+              when :always
+                new_hash[:originate][:always] = :true
+              when :metric_type, :metric
+                new_hash[:originate][attribute] = array.shift.to_i
+              else
+                new_hash[:originate][attribute] = array.shift
+              end
+            end
+          end
+          new_hash
         end
       else
         value
