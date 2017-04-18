5443060a3b1c6d722ee170ba29a8e771104769ee
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-18 10:25:46 +0300

    Added tests

--- spec/unit/type/redistribute_spec.rb
+++ spec/unit/type/redistribute_spec.rb
@@ -90,4 +90,56 @@ describe Puppet::Type.type(:redistribute) do
       expect(described_class.new(:name => 'ospf::connected', :metric => '200')[:metric]).to eq(200)
     end
   end
+
+  describe 'metric_type' do
+    it 'should support 1 as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :metric_type => 1) }.to_not raise_error
+    end
+
+    it 'should not support 3 as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :metric_type => 3) }.to raise_error(Puppet::Error, /Invalid value/)
+    end
+
+    it 'should support \'1\' as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :metric_type => '1') }.to_not raise_error
+    end
+
+    it 'should not support -1 as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :metric_type => -1) }.to raise_error(Puppet::Error, /Invalid value/)
+    end
+
+    it 'should contain 1' do
+      expect(described_class.new(:name => 'ospf::connected', :metric_type => 1)[:metric_type]).to eq(1)
+    end
+
+    it 'should contain 2' do
+      expect(described_class.new(:name => 'ospf::connected', :metric_type => '2')[:metric_type]).to eq(2)
+    end
+  end
+
+  describe 'reoute_map' do
+    it 'should support 1 as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :route_map => 1) }.to_not raise_error
+    end
+
+    it 'should not support \'ROUTE_MAP@\' as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :route_map => 'ROUTE_MAP@') }.to raise_error(Puppet::Error, /Invalid value/)
+    end
+
+    it 'should support \'route_map\' as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :route_map => '1') }.to_not raise_error
+    end
+
+    it 'should not support \'ROUTE-MAP\' as a value' do
+      expect { described_class.new(:name => 'ospf::connected', :route_map => 'ROUTE-MAP') }.to raise_error(Puppet::Error, /Invalid value/)
+    end
+
+    it 'should contain \'ROUTE_MAP\'' do
+      expect(described_class.new(:name => 'ospf::connected', :route_map => 'ROUTE_MAP')[:route_map]).to eq('ROUTE_MAP')
+    end
+
+    it 'should contain \'route_map1\'' do
+      expect(described_class.new(:name => 'ospf::connected', :route_map => 'route_map1')[:route_map]).to eq('route_map1')
+    end
+  end
 end
