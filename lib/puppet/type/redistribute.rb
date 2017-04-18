aff3e212ac866bdfc4e51094212d0d94e2c08120
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-18 09:08:01 +0300

    Added the redistribute type

---
+++ lib/puppet/type/redistribute.rb
@@ -0,0 +1,70 @@
+Puppet::Type.newtype(:redistribute) do
+  @doc = %q{
+    Example:
+
+      redistribute { 'ospf::connected':
+        metric      => 100,
+        metric_type => 2,
+        route_map   => WORD,
+      }
+
+      redistribute { 'bgp:65000:ospf':
+        metric    => 100,
+        route_map => WORD,
+      }
+  }
+
+  ensurable
+
+  newparam(:name) do
+    desc %q{ Name contains a main protocol, a id and a redistribute protocol }
+
+    newvalues(/\Aospf::(kernel|connected|static|rip|isis|bgp)\Z/)
+    newvalues(/\Abgp:\d+:(connected|kernel|ospf|rip|static)\Z/)
+  end
+
+  newproperty(:metric) do
+    desc %q{ Metric for redistributed routes }
+
+    newvalues(/\A\d+\Z/)
+
+    munge do |value|
+      value.to_i
+    end
+  end
+
+  newproperty(:metric_type) do
+    desc %q{ OSPF exterior metric type for redistributed routes }
+
+    newvalues(/\A(1|2)\Z/)
+
+    munge do |value|
+      value.to_i
+    end
+  end
+
+  newproperty(:route_map) do
+    desc %q{ Route map reference }
+
+    newvalues(/\A\w+\Z/)
+  end
+
+  autorequire(:package) do
+    case value(:provider)
+      when :quagga
+        %w{quagga}
+      else
+        []
+    end
+  end
+
+  autorequire(:service) do
+    protocol = value(:name).split(/:/).first
+    case value(:provider)
+      when :quagga
+        [ "zebra", protocol ]
+      else
+        []
+    end
+  end
+end
