dbc8b3e9a1c986add39fecca4c7fd25b8ffbd00e
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-18 10:07:08 +0300

    Added methods: instances and prefetch

--- lib/puppet/provider/redistribute/quagga.rb
+++ lib/puppet/provider/redistribute/quagga.rb
@@ -1,3 +1,49 @@
 Puppet::Type.type(:redistribute).provide :quagga do
   @doc = %q{ Manages redistribution using quagga }
+
+  commands :vtysh => 'vtysh'
+
+  def self.instances
+    debug 'Create instances of the redistribution'
+
+    redistributes = []
+    found_router = false
+    config = vtysh('-c', 'show running-config')
+    config.split(/\n/).collect do |line|
+      next if line =~ /\A!\Z/
+      if line =~ /\Arouter (ospf|bgp)( (\d+))?\Z/
+        main_protocol = $1
+        as = $3
+        as = as.to_i unless as.nil?
+        found_router = true
+      elsif line =~ /\A\w/ && found_router
+        found_router = false
+      elsif line =~ /\A\sredistribute (\w+)( metric (\d+))?( metric-type (\d))?( route-map (\w+))?\Z/ && found_router
+        protocol = $1
+        metric = $3
+        metric_type = $5
+        route_map = $7
+
+        hash = {}
+        hash[:ensure] = :present
+        hash[:provider] = self.name
+        hash[:name] = "#{main_protocol}:#{as}:#{protocol}"
+        hash[:metric] = metric.to_i unless metric.nil?
+        hash[:metric_type] = metric_type.nil? ? 2 : metric_type.to_i
+        hash[:route_map] = route_map
+        redistributes << new(hash)
+      end
+    end
+    redistributes
+  end
+
+  def self.prefetch
+    providers = instances
+    found_providers = []
+    resources.keys.each do |name|
+      if provider = providers.find { |provider| provider.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
 end
