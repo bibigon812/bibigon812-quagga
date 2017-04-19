b33c14bc84bf0f4373651cd4dd25b014372587ad
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-19 15:05:49 +0300

    Removed unused tests

--- spec/unit/type/ospf_spec.rb
+++ spec/unit/type/ospf_spec.rb
@@ -28,8 +28,7 @@ describe Puppet::Type.type(:ospf) do
       end
     end
 
-    [:abr_type, :default_information, :redistribute, :router_id,
-    ].each do |property|
+    [:abr_type, :default_information, :router_id, ].each do |property|
       it "should have a #{property} property" do
         expect(described_class.attrtype(property)).to eq(:property)
       end
@@ -121,86 +120,6 @@ describe Puppet::Type.type(:ospf) do
 
     it 'should contain \'originate metric_type 2' do
       expect(described_class.new(:name => 'ospf', :default_information => 'originate metric-type 2')[:default_information]).to eq('originate metric-type 2')
-    end
-  end
-
-  # describe 'area' do
-  #   it 'should not support \'0.0.0.0\' as a value' do
-  #     expect { described_class.new(:name => 'ospf', :area => '0.0.0.0') }.to raise_error(Puppet::Error, /Invalid value/)
-  #   end
-  #
-  #   it 'should support \'0.0.0.0 authentication message-digest\' as a value' do
-  #     expect { described_class.new(:name => 'ospf', :area => '0.0.0.0 authentication message-digest') }.to_not raise_error
-  #   end
-  #
-  #   it 'should support \'0.0.0.0 default-cost 100\' as a value' do
-  #     expect { described_class.new(:name => 'ospf', :area => '0.0.0.0 default-cost 100') }.to_not raise_error
-  #   end
-  #
-  #   it 'should support \'0.0.0.0 export-list ABCD\' as a value' do
-  #     expect { described_class.new(:name => 'ospf', :area => '0.0.0.0 export-list ABCD') }.to_not raise_error
-  #   end
-  #
-  #   it 'should support \'0.0.0.0 filter-list prefix ABCD-EF\' as a value' do
-  #     expect { described_class.new(:name => 'ospf', :area => '0.0.0.0 filter-list prefix ABCD-EF') }.to_not raise_error
-  #   end
-  #
-  #   it 'should contain \'0.0.0.0 nssa\'' do
-  #     expect(described_class.new(:name => 'ospf', :area => '0.0.0.0 nssa')[:area]).to eq('0.0.0.0 nssa')
-  #   end
-  #
-  #   it 'should contain \'0.0.0.0 nssa translate-always no-summary\'' do
-  #     expect(described_class.new(:name => 'ospf', :area => '0.0.0.0 nssa translate-always no-summary')[:area]).to eq('0.0.0.0 nssa translate-always no-summary')
-  #   end
-  # end
-
-  describe 'redistribute' do
-    it 'should support a String as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'connected route-map ABCD-EF') }.to_not raise_error
-    end
-
-    it 'should support a String as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'connected') }.to_not raise_error
-    end
-
-    it 'should not support \'bgp metric-type 5\' as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'bgp metric-type 5') }.to raise_error(Puppet::Error, /Invalid value/)
-    end
-
-    it 'should support [ \'bgp metric 100\', \'connected\' ] as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => [ 'bgp metric 100', 'connected' ]) }.to_not raise_error
-    end
-
-    it 'should support [ \'bgp metric-type 2\', \'rip route-map ABCD-EF\' ] as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => [ 'bgp metric-type 2', 'rip route-map ABCD-EF' ]) }.to_not raise_error
-    end
-
-    it 'should support \'bgp metric 10\' as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'bgp metric 10') }.to_not raise_error
-    end
-
-    it 'should support \'bgp route-map ABCD-EF\' as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'bgp route-map ABCD-EF') }.to_not raise_error
-    end
-
-    it 'should support \'bgp metric 10 metric-type 2 route-map ABCD-EF\' as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => 'bgp metric 10 metric-type 2 route-map ABCD-EF') }.to_not raise_error
-    end
-
-    it 'should support [ \'bgp metric 10\' , \'connected\' ] as a value' do
-      expect { described_class.new(:name => 'ospf', :redistribute => [ 'bgp metric 10', 'connected' ]) }.to_not raise_error
-    end
-
-    it 'should contain \'bgp metric 10 metric-type 2 route-map ABCD-EF\'' do
-      expect(described_class.new(:name => 'ospf', :redistribute => 'bgp metric 10 metric-type 2 route-map ABCD-EF')[:redistribute]).to eq([ 'bgp metric 10 metric-type 2 route-map ABCD-EF' ])
-    end
-
-    it 'should contain \'bgp metric 10 metric-type  2 route-map ABCD-EF\'' do
-      expect(described_class.new(:name => 'ospf', :redistribute => 'bgp metric 10 metric-type 2 route-map ABCD-EF')[:redistribute]).to eq([ 'bgp metric 10 metric-type 2 route-map ABCD-EF' ])
-    end
-
-    it 'should contain [ \'bgp metric 10 metric-type 2 route-map ABCD-EF\', \'connected\' ]' do
-      expect(described_class.new(:name => 'ospf', :redistribute => [ 'bgp metric 10 metric-type 2 route-map ABCD-EF', 'connected' ])[:redistribute]).to eq([ 'bgp metric 10 metric-type 2 route-map ABCD-EF', 'connected' ])
     end
   end
 
