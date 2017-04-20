4a6f746f1ba2c242baf4c17a7e15a74033a96463
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-20 09:57:06 +0300

    Added properties

--- lib/puppet/type/ospf_area.rb
+++ lib/puppet/type/ospf_area.rb
@@ -5,9 +5,10 @@ Puppet::Type.newtype(:ospf_area) do
 
       ospf_area { '0.0.0.0':
         default_cost  => 10,
-        export_list   => EXPORT_ACCESS_LIST,
-        filter_prefix => FILTER_PREFIX_LIST,
-        import_list   => IPMORT_ACCESS_LIST,
+        list_export   => EXPORT_ACCESS_LIST,
+        list_import   => IPMORT_ACCESS_LIST,
+        prefix_export => EXPORT_PREFIX_LIST,
+        prefix_import => IMPORT_PREFIX_LIST,
         network       => [ 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16 ],
         shortcut      => default,
       }
@@ -25,9 +26,67 @@ Puppet::Type.newtype(:ospf_area) do
 
   newparam(:name) do
     desc %q{ OSPF area }
-    newvalues /\A\d+\.\d+\.\d+\.\d+\Z/
+
+    block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
+    re = /\A#{block}\.#{block}\.#{block}\.#{block}\Z/
+
+    newvalues(re)
   end
 
-  # TODO
-  #
+  newproperty(:default_cost) do
+    desc %q{ Set the summary-default cost of a NSSA or stub area }
+
+    newvalues(/\A\d+\Z/)
+  end
+
+  newproperty(:list_export) do
+    desc %q{ Set the filter for networks announced to other areas }
+
+    newvalues(/\A[\w-]+\Z/)
+  end
+
+  newproperty(:list_import) do
+    desc %q{ Set the filter for networks from other areas announced to the specified one }
+
+    newvalues(/\A[\w-]+\Z/)
+  end
+
+  newproperty(:prefix_export) do
+    desc %q{ Filter networks sent from this area }
+
+    newvalues(/\A[\w-]+\Z/)
+  end
+
+  newproperty(:prefix_import) do
+    desc %q{ Filter networks sent to this area }
+
+    newvalues(/\A[\w-]+\Z/)
+  end
+
+  newproperty(:network, :array_matching => :all) do
+    desc %q{ Enable routing on an IP network }
+
+    block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
+    re = /\A#{block}\.#{block}\.#{block}\.#{block}\/(\d|[1-2][\d]|3[0-2])\Z/
+
+    newvalues(re)
+  end
+
+  newproperty(:shortcut) do
+    desc %q{ Configure the area's shortcutting mode }
+
+    newvalues(:false, :true, :default)
+    defaultto(:default)
+  end
+
+  newproperty(:stub) do
+    desc %q{ Configure OSPF area as stub }
+
+    newvalues(:false, :true, :no_summary, 'no-summary')
+    defaultto(:false)
+
+    munge do |value|
+      value.to_s.gsub(/-/, '_').to_sym if value == 'no-summary'
+    end
+  end
 end
