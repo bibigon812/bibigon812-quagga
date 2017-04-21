1c5752476136248e8a031d16421ad8f04072f3a3
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-21 14:49:02 +0300

    Updated the ensurable method

--- lib/puppet/type/redistribution.rb
+++ lib/puppet/type/redistribution.rb
@@ -14,7 +14,17 @@ Puppet::Type.newtype(:redistribution) do
       }
   }
 
-  ensurable
+  ensurable do
+    newvalues(:present) do
+      provider.create
+    end
+
+    newvalues(:absent) do
+      provider.destroy
+    end
+
+    defaultto(:present)
+  end
 
   newparam(:name) do
     desc %q{ The name contains the main protocol, the id and the the protocol for redistribution }
