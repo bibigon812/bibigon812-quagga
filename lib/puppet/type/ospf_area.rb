614c30cebe0216a0fe246960674ca8aacf90e6b1
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-26 09:26:40 +0300

    Updated a munge method

--- lib/puppet/type/ospf_area.rb
+++ lib/puppet/type/ospf_area.rb
@@ -123,9 +123,9 @@ Puppet::Type.newtype(:ospf_area) do
 
     munge do |value|
       case value
-        when :false, 'false', false
+        when :false, 'false', false, 'disable'
           :disable
-        when :true, 'true', true
+        when :true, 'true', true, 'enable'
           :enable
         when 'no-summary', 'no_summary'
           :no_summary
