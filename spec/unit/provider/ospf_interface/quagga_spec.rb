80d103dd7779a869ff2738df7fd040cd93c3d109
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-27 09:35:40 +0300

    Updated tests

--- spec/unit/provider/ospf_interface/quagga_spec.rb
+++ spec/unit/provider/ospf_interface/quagga_spec.rb
@@ -16,44 +16,61 @@ describe Puppet::Type.type(:ospf_interface).provider(:quagga) do
   context 'with three interfaces' do
     before :each do
       described_class.expects(:vtysh).with(
-        '-c', 'show ip ospf interface'
-      ).returns 'lo is up
-  ifindex 1, MTU 65536 bytes, BW 0 Kbit <UP,LOOPBACK,RUNNING>
-  OSPF not enabled on this interface
-tun0 is up
-  ifindex 45, MTU 1476 bytes, BW 0 Kbit <UP,POINTOPOINT,RUNNING,NOARP>
-  OSPF not enabled on this interface
-tun1 is up
-  ifindex 44, MTU 1476 bytes, BW 0 Kbit <UP,POINTOPOINT,RUNNING,NOARP>
-  Internet Address 10.255.8.49/30, Area 0.0.0.0
-  MTU mismatch detection:disabled
-  Router ID 91.228.177.2, Network Type BROADCAST, Cost: 100
-  Transmit Delay is 1 sec, State Backup, Priority 100
-  Designated Router (ID) 10.255.78.1, Interface Address 10.255.8.50
-  Backup Designated Router (ID) 91.228.177.2, Interface Address 10.255.8.49
-  Multicast group memberships: OSPFAllRouters OSPFDesignatedRouters
-  Timer intervals configured, Hello 2s, Dead 8s, Wait 8s, Retransmit 4
-    Hello due in 0.584s
-  Neighbor Count is 1, Adjacent neighbor count is 1'
+        '-c', 'show running-config'
+      ).returns 'interface eth0
+!
+interface eth1
+ ip ospf hello-interval 2
+ ip ospf dead-interval 8
+ ip ospf priority 50
+ ip ospf retransmit-interval 4
+ ip ospf mtu-ignore
+!
+interface gre0
+!
+interface gretap0
+!
+interface ip_vti0
+!
+interface lo
+!
+interface tun0
+!'
     end
 
     it 'should return a resource' do
-      expect(described_class.instances.size).to eq(1)
+      expect(described_class.instances.size).to eq(7)
     end
 
-    it 'should return the resource tun1' do
+    it 'should return the resource eth0' do
       expect(described_class.instances[0].instance_variable_get('@property_hash')).to eq({
         :ensure => :present,
-        :name => 'tun1',
+        :name => 'eth0',
         :provider => :quagga,
-        :cost => 100,
-        :dead_interval => 8,
-        :hello_interval => 2,
-        :mtu_ignore => :enable,
+        :cost => 10,
+        :dead_interval => 40,
+        :hello_interval => 10,
+        :mtu_ignore => :disable,
         :network => :broadcast,
-        :priority => 100,
-        :retransmit_interval => 4,
+        :priority => 1,
+        :retransmit_interval => 5,
         :transmit_delay => 1,
+      })
+    end
+
+    it 'should return the resource eth1' do
+      expect(described_class.instances[1].instance_variable_get('@property_hash')).to eq({
+         :ensure => :present,
+         :name => 'eth1',
+         :provider => :quagga,
+         :cost => 10,
+         :dead_interval => 8,
+         :hello_interval => 2,
+         :mtu_ignore => :enable,
+         :network => :broadcast,
+         :priority => 50,
+         :retransmit_interval => 4,
+         :transmit_delay => 1,
       })
     end
   end
