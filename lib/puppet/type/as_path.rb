5e0dd3a2fe0edaeff402e16eb50d221f8e409234
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-04-28 12:50:34 +0300

    Changed a type structure

--- lib/puppet/type/as_path.rb
+++ lib/puppet/type/as_path.rb
@@ -1,35 +1,18 @@
 Puppet::Type.newtype(:as_path) do
-  @doc = %q{This type provides the capabilities to manage as-path access-list
+  @doc = %q{ This type provides the capabilities to manage as-path access-list
     within puppet.
 
     Example:
 
-    as_path { 'from_as100:1':
-      ensure => present,
-      action => 'permit',
-      regex  => '_100$',
-    }
+    as_path { 'from_as100:permit:_100$': }
   }
 
   ensurable
 
   newparam(:name) do
-    desc %q{ The name contains the as-path name and sequence number }
+    desc %q{ The name contains the as-path name, action and regex }
 
-    newvalues(/\A\w+:\d+\Z/)
-  end
-
-  newproperty(:action) do
-    desc %q{ Action permit or deny }
-
-    defaultto(:permit)
-    newvalues(:deny, :permit)
-  end
-
-  newproperty(:regex) do
-    desc %q{ A regular-expression to match the BGP AS paths }
-
-    newvalues(/\A[_\^\\]?[_\d\.\\\*\+\[\]\|\?]+[_\$\\]?\Z/)
+    newvalues(/\A\w+:(deny|permit):\^?[_\d\.\\\*\+\[\]\|\?]+\$?\Z/)
   end
 
   autorequire(:package) do
@@ -48,16 +31,5 @@ Puppet::Type.newtype(:as_path) do
       else
         []
     end
-  end
-
-  autorequire(:as_path) do
-    as_paths = []
-    name, sequence = value(:name).split(/:/)
-
-    (1...sequence).each do |seq|
-      as_paths << "#{name}:#{seq}"
-    end
-
-    as_paths
   end
 end
--- spec/unit/type/as_path_spec.rb
+++ spec/unit/type/as_path_spec.rb
@@ -27,79 +27,35 @@ describe Puppet::Type.type(:as_path) do
         expect(described_class.attrtype(param)).to eq(:param)
       end
     end
-
-    [:action, :regex].each do |property|
-      it "should have a #{property} property" do
-        expect(described_class.attrtype(property)).to eq(:property)
-      end
-    end
   end
 
   describe 'when validating values' do
     describe 'ensure' do
       it 'should support present as a value' do
-        expect { described_class.new(:name => 'from_as100:1', :ensure => :present) }.to_not raise_error
+        expect { described_class.new(:name => 'from_as100:permit:_100$', :ensure => :present) }.to_not raise_error
       end
 
       it 'should support absent as a value' do
-        expect { described_class.new(:name => 'from_as100:1', :ensure => :absent) }.to_not raise_error
+        expect { described_class.new(:name => 'from_as100:permit:_100$', :ensure => :absent) }.to_not raise_error
       end
 
       it 'should not support other values' do
-        expect { described_class.new(:name => 'from_as100:1', :ensure => :foo) }.to raise_error(Puppet::Error, /Invalid value/)
+        expect { described_class.new(:name => 'from_as100:permit:_100$', :ensure => :foo) }.to raise_error(Puppet::Error, /Invalid value/)
       end
     end
   end
 
-  describe 'action' do
-    it 'should support permit as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :action => :permit) }.to_not raise_error
+  describe 'name' do
+    it 'should support from_as100:permit:^100$ as a value' do
+      expect { described_class.new(:name => 'from_as100:permit:^100$') }.to_not raise_error
     end
 
-    it 'should support deny as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :action => :deny) }.to_not raise_error
+    it 'should support from_as100:permit:^100_100$ as a value' do
+      expect { described_class.new(:name => 'from_as100:deny:^100_100$') }.to_not raise_error
     end
 
     it 'should not support accept as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :action => :accept) }.to raise_error(Puppet::Error, /Invalid value/)
-    end
-
-    it 'should contain permit' do
-      expect(described_class.new(:name => 'from_as100:1', :action => :permit)[:action]).to eq(:permit)
-    end
-
-    it 'should contain permit' do
-      expect(described_class.new(:name => 'from_as100:1', :action => 'permit')[:action]).to eq(:permit)
-    end
-  end
-
-  describe 'regex' do
-    it 'should support _100$ as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :regex => '_100$') }.to_not raise_error
-    end
-
-    it 'should support _100_200.$ as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :regex => '_100_200.$') }.to_not raise_error
-    end
-
-    it 'should support ^100_100$ as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :regex => '^100_\\100$') }.to_not raise_error
-    end
-
-    it 'should support ^100*_100+$ as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :regex => '^100*_100_$') }.to_not raise_error
-    end
-
-    it 'should not support ^100^_100$ as a value' do
-      expect { described_class.new(:name => 'from_as100:1', :regex => '^100^_100$') }.to raise_error(Puppet::Error, /Invalid value/)
-    end
-
-    it 'should contain _100_100$' do
-      expect(described_class.new(:name => 'from_as100:1', :regex => '_100_100$')[:regex]).to eq('_100_100$')
-    end
-
-    it 'should contain ^100_200.$' do
-      expect(described_class.new(:name => 'from_as100:1', :regex => '^100_200.$')[:regex]).to eq('^100_200.$')
+      expect { described_class.new(:name => 'from_as100:1') }.to raise_error(Puppet::Error, /Invalid value/)
     end
   end
 end
