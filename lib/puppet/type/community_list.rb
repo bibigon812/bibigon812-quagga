1f63d105131061b79a0989a40f19e5361b626aa9
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-05-02 10:25:33 +0300

    Added the community_list type

---
+++ lib/puppet/type/community_list.rb
@@ -0,0 +1,77 @@
+Puppet::Type.newtype(:community_list) do
+  @doc = %q{
+    This type provides the capability to manage community-list
+    within puppet.
+
+    Example:
+
+      community_list {'100':
+        ensure => present,
+        rules  => [
+          permit => 65000:50952,
+          permit => 65000:31500,
+        ],
+      }
+  }
+
+  ensurable
+
+  newparam(:name) do
+    desc %q{ Community list number. }
+
+    newvalues(/^\d+$/)
+
+    validate do |value|
+      value_i = value.to_i
+      if value_i < 1 or value_i > 500
+        raise ArgumentError, 'Community list number: 1-500'
+      end
+    end
+  end
+
+  newproperty(:rules, :array_matching => :all) do
+    desc %q{ Action and community. }
+
+    validate do |value|
+      case value
+        when Hash
+          value.each do |action, community|
+            unless [:deny, :permit].include?(action.to_s.to_sym)
+              raise(ArgumentError, "Use the action permit or deny instead of #{action}")
+            end
+            unless community.match(/\A\d+:\d+\Z/)
+              raise(ArgumentError, "The community #{community} is invalid")
+            end
+          end
+        else
+          raise(ArgumentError, 'Use a hash { action => community }')
+      end
+    end
+
+    munge do |value|
+      new_value = {}
+      value.each do |action, community|
+        new_value[action.to_s.to_sym] = community
+      end
+      new_value
+    end
+  end
+
+  autorequire(:package) do
+    case value(:provider)
+      when :quagga
+        %w{quagga}
+      else
+        []
+    end
+  end
+
+  autorequire(:service) do
+    case value(:provider)
+      when :quagga
+        %w{zebra bgpd}
+      else
+        []
+    end
+  end
+end
---
+++ spec/unit/type/community_list_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:community_list) do
+  let(:networking_service) do
+    @provider_class = describe_class.provide(:community_list) {
+      mk_resource_methods
+    }
+    @provider_class.stub(:suitable?).return true
+    @provider_class
+  end
+
+  before :each do
+    described_class.stubs(:defaultprovider).returns @provider_class
+  end
+
+  after :each do
+    described_class.unprovide(:community_list)
+  end
+
+  it 'should have :name be its namevar' do
+    expect(described_class.key_attributes).to eq([:name])
+  end
+
+  describe "when validating attributes" do
+    [:name, :provider].each do |param|
+      it "should have a #{param} parameter" do
+        expect(described_class.attrtype(param)).to eq(:param)
+      end
+    end
+  end
+
+  describe 'when validating values' do
+    describe 'ensure' do
+      it 'should support present as a value' do
+        expect { described_class.new(:name => '100', :ensure => :present) }.to_not raise_error
+      end
+
+      it 'should support absent as a value' do
+        expect { described_class.new(:name => '100', :ensure => :absent) }.to_not raise_error
+      end
+
+      it 'should not support other values' do
+        expect { described_class.new(:name => '100', :ensure => :foo) }.to raise_error(Puppet::Error, /Invalid value/)
+      end
+    end
+  end
+
+  describe 'name' do
+    it 'should support as100 as a value' do
+      expect { described_class.new(:name => '100') }.to_not raise_error
+    end
+
+    it 'should support as100 as a value' do
+      expect { described_class.new(:name => '100') }.to_not raise_error
+    end
+
+    it 'should not support as100 as a value' do
+      expect { described_class.new(:name => 'as100') }.to raise_error(Puppet::Error, /Community list number: 1-500/)
+    end
+  end
+
+  describe 'rules' do
+    it 'should support :premit => \'65000:1\' as a value' do
+      expect { described_class.new(:name => '100', :rules => { :permit => '65000:1' }) }.to_not raise_error
+    end
+
+    it 'should support [{:permit => \'65000:1\'}, {:permit => \'65000:2\'}] as a value' do
+      expect { described_class.new(:name => '100', :rules => [{:permit => '65000:1'}, {:permit => '65000:2'}]) }.to_not raise_error
+    end
+
+    it 'should not support [{:permit => \'AS65000:1\'}, {:permit => \'65000:2\'}] as a value' do
+      expect { described_class.new(:name => '100', :rules => [{:permit => 'AS65000:1'}, {:permit => '65000:2'}]) }.to raise_error(Puppet::Error, /The community AS65000:1 is invalid/)
+    end
+
+    it 'should not support [{:reject => \'65000:1\'}, {:permit => \'65000:2\'}] as a value' do
+      expect { described_class.new(:name => '100', :rules => [{:reject => '65000:1'}, {:permit => '65000:2'}]) }.to raise_error(Puppet::Error, /Use the action permit or deny instead of reject/)
+    end
+
+    it 'should not support a string as a value' do
+      expect { described_class.new(:name => '100', :rules => 'permit => 65000:1') }.to raise_error(Puppet::Error, /Use a hash { action => community }/)
+    end
+
+    it 'should contain [{:permit => \'65000:1\'}]' do
+      expect(described_class.new(:name => '100', :rules => {:permit => '65000:1'})[:rules]).to eq([{:permit => '65000:1'}])
+    end
+
+    it 'should contain [{:permit => \'65000:1\'}, {:permit => \'65000:2\'}]' do
+      expect(described_class.new(:name => '100', :rules => [{:permit => '65000:1'}, {:permit => '65000:2'}])[:rules]).to eq([{:permit => '65000:1'}, {:permit => '65000:2'}])
+    end
+  end
+end
