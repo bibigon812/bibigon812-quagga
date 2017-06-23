fc46ceb574d4c00976507aa544569c6aede26c1e
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-06-23 05:44:37 +0300

    Fixed typos

--- manifests/bgp.pp
+++ manifests/bgp.pp
@@ -35,7 +35,7 @@ class quagga::bgp (
     }
 
     unless empty($bgp_peers) {
-      create_resources('quagga_bgp_peers', $bgp_peers)
+      create_resources('quagga_bgp_peer', $bgp_peers)
     }
 
     unless empty($redistribution) {
