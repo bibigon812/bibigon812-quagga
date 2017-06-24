be22a3afbbf876d150bdcbf674d87abb8e78a703
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-06-24 08:47:37 +0300

    Removed creating the resource `quagga_redistribution`

--- manifests/ospf.pp
+++ manifests/ospf.pp
@@ -3,24 +3,10 @@ class quagga::ospf (
 ) {
   $ensure = dig44($settings, ['ensure'], 'present')
 
-  $ospf = { 'ospf' => delete(delete($settings, 'redistribute'), 'areas') }
+  $ospf = { 'ospf' => delete($settings, 'areas') }
 
   $ospf_areas = dig44($settings, ['areas'], {}).reduce({}) |$memo, $value| {
     merge($memo, { $value[0] => merge({ ensure => $ensure }, $value[1]) })
-  }
-
-  $redistribution = dig44($settings, ['redistribute'], {}).reduce({}) |$memo, $value| {
-    $name = $value ? {
-      Hash    => $value.keys[0],
-      default => $value,
-    }
-
-    $config = $value ? {
-      Hash    => $value.values[0],
-      default => {},
-    }
-
-    merge($memo, { "ospf::${name}" => merge({ ensure => $ensure }, $config) })
   }
 
   unless empty($ospf) {
@@ -30,6 +16,4 @@ class quagga::ospf (
   unless empty($ospf_areas) {
     create_resources('quagga_ospf_area', $ospf_areas)
   }
-
-  create_resources('quagga_redistribution', $redistribution)
 }
