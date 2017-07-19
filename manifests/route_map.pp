39f1f0afda3890312535e579e9851115e55d6317
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-07-19 13:13:30 +0300

    Fixed alignment

--- manifests/route_map.pp
+++ manifests/route_map.pp
@@ -2,7 +2,7 @@ define quagga::route_map (
   Hash $rules = {},
 ) {
   $rules.reduce({}) |Hash $rules, Tuple[Integer, Hash] $rule| {
-      merge($rules, { "${name} ${rule[0]}" => $rule[1] })
+    merge($rules, { "${name} ${rule[0]}" => $rule[1] })
   }.each |String $route_map_name, Hash $route_map| {
     quagga_route_map {$route_map_name:
       * => $route_map,
