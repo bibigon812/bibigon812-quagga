bc05d76a31c392334e7300c914eabd892c31af71
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-08-21 07:07:52 +0300

    Added the resource type `quagga_static_route`

---
+++ lib/puppet/type/quagga_static_route.rb
@@ -0,0 +1,112 @@
+Puppet::Type.newtype(:quagga_static_route) do
+  @doc = %q{
+    This type provides the capability to manage static routes within puppet.
+
+      Example:
+
+        quagga_static_route {'172.16.2.0/24':
+          ensure      => present,
+          hexthop     => '192.168.1.10',
+          distance    => 10,
+        }
+
+        quagga_static_route {'172.16.3.0/24':
+          ensure      => present,
+          hexthop     => 'null0',
+          distance    => 10,
+        }
+  }
+
+  def self.title_patterns
+    [
+      [
+        /\A(\S+)\Z/,
+        [
+          [:prefix],
+        ],
+      ],
+      [
+        /\A(\S+)\s+(\S+)\Z/,
+        [
+          [:prefix],
+          [:nexthop],
+        ],
+      ],
+    ]
+  end
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:prefix, namevar: true) do
+    desc 'IP destination prefix.'
+
+    validate do |value|
+      begin
+        IPAddr.new(value)
+      rescue
+        fail "Not a valid ip address '#{value}'"
+      end
+      fail "Prefix length is not specified '#{value}'" unless value.include?('/')
+    end
+
+    munge do |value|
+      prefix, length = value.split('/')
+      network = IPAddr.new(value)
+      "#{network.to_s}/#{length}"
+    end
+  end
+
+  newparam(:nexthop, namevar: true) do
+    desc 'Specifies IP or the interface name of the nexthop router.'
+
+    defaultto 'null0'
+
+    validate do |value|
+      if value != 'null0'
+        if value.include?('/')
+          fail 'Do not specify the prefix length \'%{value}\'' % { value: value }
+        else
+          begin
+            IPAddr.new(value)
+          rescue
+            unless Facter.value(:interfaces).split(',').include?(value)
+              fail 'The network interface \'%{value}\' was not found'
+            end
+          end
+        end
+      end
+    end
+  end
+
+  newproperty(:distance) do
+    desc 'Specifies the distance value for this route.'
+
+    defaultto(:absent)
+
+    validate do |value|
+      unless value == :absent
+        fail "Invalid value '#{value}'. It is not an Integer" unless value.is_a?(Integer)
+        fail 'Invalid value. Maximum prefix length: 1-32' unless value >= 1 and value <= 255
+      end
+    end
+  end
+
+  newproperty(:option) do
+    desc 'Set reject or blackhole for route.'
+
+    defaultto :absent
+    newvalues(:absent, :blackhole, :reject)
+  end
+
+
+  autorequire(:package) do
+    %w{quagga}
+  end
+
+  autorequire(:service) do
+    %w{zebra}
+  end
+end
---
+++ spec/unit/provider/quagga_static_route/quagga_spec.rb
@@ -0,0 +1,128 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:quagga_static_route).provider(:quagga) do
+  describe 'instances' do
+    it 'should have an instance method' do
+      expect(described_class).to respond_to :instances
+    end
+  end
+
+  describe 'prefetch' do
+    it 'should have a prefetch method' do
+      expect(described_class).to respond_to :prefetch
+    end
+  end
+
+  context 'running-config' do
+    before :each do
+      described_class.expects(:vtysh).with(
+        '-c', 'show running-config'
+      ).returns '!
+hostname router-1.sandbox.local
+!
+ip forwarding
+ipv6 forwarding
+!
+ip multicast-routing
+!
+ip route 10.0.0.0/8 10.1.2.1 blackhole
+ip route 10.0.0.0/8 10.1.1.1
+ip route 10.0.0.0/8 null0 250
+!
+line vty
+!
+end'
+    end
+
+    it 'should return a resource' do
+      expect(described_class.instances.size).to eq(3)
+    end
+
+    it 'should return the resource `quagga_statis_route`' do
+      expect(described_class.instances[0].instance_variable_get('@property_hash')).to eq({
+        distance: :absent,
+        ensure: :present,
+        nexthop: '10.1.2.1',
+        option: :blackhole,
+        prefix: '10.0.0.0/8',
+        provider: :quagga,
+      })
+    end
+
+    it 'should return the resource `quagga_statis_route`' do
+      expect(described_class.instances[1].instance_variable_get('@property_hash')).to eq({
+        distance: :absent,
+        ensure: :present,
+        nexthop: '10.1.1.1',
+        option: :absent,
+        prefix: '10.0.0.0/8',
+        provider: :quagga,
+      })
+    end
+
+    it 'should return the resource `quagga_statis_route`' do
+      expect(described_class.instances[2].instance_variable_get('@property_hash')).to eq({
+        distance: 250,
+        ensure: :present,
+        nexthop: 'null0',
+        option: :absent,
+        prefix: '10.0.0.0/8',
+        provider: :quagga,
+      })
+    end
+  end
+
+  let(:provider) do
+    described_class.new(
+        ensure:   :present,
+        provider: :iproute2,
+    )
+  end
+
+  let(:catalog) { Puppet::Resource::Catalog.new }
+
+  describe 'prefetch' do
+    before :each do
+      described_class.stubs(:vtysh).with('-c', 'show running-config').
+          returns <<-EOS
+ip route 172.16.3.0/24 null0
+ip route 172.16.3.0/24 172.16.0.4
+ip route 172.16.3.0/24 172.16.0.5 blackhole
+ip route 172.16.3.0/24 172.16.0.6 250
+EOS
+    end
+
+    context 'network_route \'172.16.3.0/24\'' do
+      let(:resources) do
+        hash = {}
+          %w{null0 172.16.0.4 172.16.0.5 172.16.0.6}.each do |nexthop|
+            hash["172.16.3.0/24 #{nexthop}"] = Puppet::Type.type(:quagga_static_route).new(title: "172.16.3.0/24 #{nexthop}")
+          end
+        hash
+      end
+
+      it 'with nexthop ' do
+        described_class.prefetch(resources)
+        expect(resources['172.16.3.0/24 null0'].provider.prefix).to eq('172.16.3.0/24')
+        expect(resources['172.16.3.0/24 null0'].provider.nexthop).to eq('null0')
+        expect(resources['172.16.3.0/24 null0'].provider.distance).to eq(:absent)
+        expect(resources['172.16.3.0/24 null0'].provider.option).to eq(:absent)
+
+        expect(resources['172.16.3.0/24 172.16.0.4'].provider.prefix).to eq('172.16.3.0/24')
+        expect(resources['172.16.3.0/24 172.16.0.4'].provider.nexthop).to eq('172.16.0.4')
+        expect(resources['172.16.3.0/24 172.16.0.4'].provider.distance).to eq(:absent)
+        expect(resources['172.16.3.0/24 172.16.0.4'].provider.option).to eq(:absent)
+
+        expect(resources['172.16.3.0/24 172.16.0.5'].provider.prefix).to eq('172.16.3.0/24')
+        expect(resources['172.16.3.0/24 172.16.0.5'].provider.nexthop).to eq('172.16.0.5')
+        expect(resources['172.16.3.0/24 172.16.0.5'].provider.distance).to eq(:absent)
+        expect(resources['172.16.3.0/24 172.16.0.5'].provider.option).to eq(:blackhole)
+
+        expect(resources['172.16.3.0/24 172.16.0.6'].provider.prefix).to eq('172.16.3.0/24')
+        expect(resources['172.16.3.0/24 172.16.0.6'].provider.nexthop).to eq('172.16.0.6')
+        expect(resources['172.16.3.0/24 172.16.0.6'].provider.distance).to eq(250)
+        expect(resources['172.16.3.0/24 172.16.0.6'].provider.option).to eq(:absent)
+      end
+    end
+  end
+end
