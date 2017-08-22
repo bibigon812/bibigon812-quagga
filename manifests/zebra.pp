11a53d935e3ce690155d473e39db208ede5e1580
Dmitriy Yakovlev <yak0vl3v@gmail.com>
2017-08-22 14:47:26 +0300

    Added the creating `quagga_access_list`

--- manifests/zebra.pp
+++ manifests/zebra.pp
@@ -7,14 +7,26 @@ class quagga::zebra (
   Enum['running', 'stopped'] $service_ensure,
   String $service_opts,
   Hash $routes,
+  Hash $access_lists,
 ) {
   include quagga::zebra::config
   include quagga::zebra::service
 
   if $service_enable and $service_ensure == 'running' {
-    $routes.each | $route_title, $route | {
+    $routes.each |String $route_title, Hash $route| {
       quagga_static_route { $route_title:
-        * => $route
+        * => $route,
+      }
+    }
+
+    $access_lists.each |$access_list_name, Hash $access_list| {
+      $list_name = $access_list_name ? {
+        Integer => sprintf('%d', $access_list_name),
+        default => $access_list_name,
+      }
+
+      quagga_access_list { $list_name:
+        * => $access_list,
       }
     }
   }
